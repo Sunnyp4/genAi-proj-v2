40cd31afc56293134f69dc2c9c5b31b0c7a4398c
Sunny Prakash <sunny.prakash@tigeranalytics.local>
2026-05-26 23:23:11 +0530

    Initial commit6

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const app=require('./src/app')
 const connectDB=require('./src/config/db')
 const cookieParser=require('cookie-parser');
+const cors = require('cors')
 
 app.use(cors({
   origin: [
